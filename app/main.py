ee8713f619046443003d2c295069ada04c2891b5
ablott976 <ablott@gmail.com>
2026-03-07 05:06:08 +0100

    wip(#3): Register services router in app/main.py [11/13]

---
+++ app/main.py
@@ -0,0 +1,12 @@
+"""Application entrypoint."""
+
+from __future__ import annotations
+
+from fastapi import FastAPI
+
+from app.api.health import router as health_router
+from app.api.services import router as services_router
+
+app = FastAPI()
+app.include_router(health_router)
+app.include_router(services_router)
